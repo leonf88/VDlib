7fabf4dd4138f4412b23f71dc19a0eb3ccb3c42e
liangfan <chcdlf@gmail.com>
2013-06-04 16:42:24 +0800

    Initialize VDlib

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VDlib::Application.config.secret_token = '98ace78e7268e12028c67c08408b04bd21dc57209f78f765927938d004bf244973b639aaca967a6302ec909f72b1064c2b34ec8ac171c8362436643c5be5cee1'
